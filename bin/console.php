f418625966eca42eb2bae308994c3728bc3aec77
Stefan Müller <mu.stefan@googlemail.com>
2024-01-25 12:50:30 +0100

    Add command to generate file hashes and fix some file hash versions

--- bin/console.php
+++ bin/console.php
@@ -2,6 +2,7 @@
 <?php
 declare(strict_types=1);
 
+use Gared\EtherScan\Console\GenerateFileHashesCommand;
 use Gared\EtherScan\Console\GenerateRevisionLookupCommand;
 use Gared\EtherScan\Console\ScanCommand;
 use Symfony\Component\Console\Application;
@@ -11,4 +12,5 @@ require __DIR__ . '/../vendor/autoload.php';
 $application = new Application();
 $application->add(new ScanCommand());
 $application->add(new GenerateRevisionLookupCommand());
+$application->add(new GenerateFileHashesCommand());
 $application->run();
---
+++ src/Console/GenerateFileHashesCommand.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace Gared\EtherScan\Console;
+
+use Gared\EtherScan\Api\GithubApi;
+use Gared\EtherScan\Service\FileHashLookupService;
+use Gared\EtherScan\Service\RevisionLookupService;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'ether:generate-file-hashes',
+    description: 'Generate file hashes of given instance'
+)]
+class GenerateFileHashesCommand extends Command
+{
+    protected function configure()
+    {
+        $this->addArgument('url', InputArgument::REQUIRED, 'Url to etherpad instance');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $url = $input->getArgument('url');
+
+        $fileHashLookup = new FileHashLookupService();
+
+        $attributesHash = $this->getFileHash($url, 'static/js/attributes.js');
+        $padEditbarHash = $this->getFileHash($url, 'static/js/pad_editbar.js');
+        $padHash = $this->getFileHash($url, 'static/js/pad.js');
+        $padUtilsHash = $this->getFileHash($url, 'static/js/pad_utils.js');
+
+        $symfonyStyle = new SymfonyStyle($input, $output);
+        $symfonyStyle->table(['File', 'Hash', 'Version'], [
+            ['static/js/attributes.js', $attributesHash, $fileHashLookup->getEtherpadVersionRange('static/js/attributes.js', $attributesHash)],
+            ['static/js/pad_editbar.js', $padEditbarHash, $fileHashLookup->getEtherpadVersionRange('static/js/pad_editbar.js', $padEditbarHash)],
+            ['static/js/pad.js', $padHash, $fileHashLookup->getEtherpadVersionRange('static/js/pad.js', $padHash)],
+            ['static/js/pad_utils.js', $padUtilsHash, $fileHashLookup->getEtherpadVersionRange('static/js/pad_utils.js', $padUtilsHash)],
+        ]);
+
+        return self::SUCCESS;
+    }
+
+    private function getFileHash(string $url, string $path): ?string
+    {
+        try {
+            $client = new Client([
+                'base_uri' => $url,
+                'timeout' => 5.0,
+            ]);
+            $response = $client->get($path);
+            $body = (string) $response->getBody();
+            if(str_contains($body, "\n")) {
+                var_dump('NO MINIFY');
+            }
+            return hash('md5', $body);
+        } catch (GuzzleException) {
+        }
+
+        return null;
+    }
+}
