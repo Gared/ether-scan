e2bfc977d3bc4713c9c60bc2b76f8737ad95ef15
Stefan Müller <stefan@stefans-entwicklerecke.de>
2024-10-09 21:32:28 +0200

    Add specific exception if pad is not publicly available

--- src/Service/ScannerService.php
+++ src/Service/ScannerService.php
@@ -8,6 +8,7 @@ use ElephantIO\Yeast;
 use Exception;
 use Gared\EtherScan\Api\GithubApi;
 use Gared\EtherScan\Exception\EtherpadServiceNotFoundException;
+use Gared\EtherScan\Exception\EtherpadServiceNotPublicException;
 use Gared\EtherScan\Model\VersionRange;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
@@ -189,9 +190,6 @@ class ScannerService
         $this->getAdmin('user', 'changeme1', $callback);
     }
 
-    /**
-     * @throws EtherpadServiceNotFoundException
-     */
     private function scanPad(ScannerServiceCallbackInterface $callback): void
     {
         $callback->onScanPadStart();
@@ -529,7 +527,7 @@ class ScannerService
         $data = $data[1];
         $accessStatus = $data['accessStatus'] ?? null;
         if ($accessStatus === 'deny') {
-            $callback->onScanPadException(new EtherpadServiceNotFoundException('Pads are not publicly accessible'));
+            $callback->onScanPadException(new EtherpadServiceNotPublicException('Pads are not publicly accessible'));
             return;
         }
 
@@ -582,7 +580,7 @@ class ScannerService
             if ($result !== null && is_array($result->data)) {
                 $accessStatus = $result->data['accessStatus'] ?? null;
                 if ($accessStatus === 'deny') {
-                    $callback->onScanPadException(new EtherpadServiceNotFoundException('Pads are not publicly accessible'));
+                    $callback->onScanPadException(new EtherpadServiceNotPublicException('Pads are not publicly accessible'));
                     return;
                 }
                 $data = $result->data;
